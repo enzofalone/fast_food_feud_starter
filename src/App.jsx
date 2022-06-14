ed5487409cb6f3a3bbdddb89ef13354a23fa87ef
Enzo Falone <enzofalone123@gmail.com>
2022-06-13 23:18:51 -0400

    all core features implemented

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import { createDataSet } from "./data/dataset"
 import "./App.css"
 // SUB-COMPONENTS
 import Header from "./components/Header/Header"
-import Instructions from "./components/Instructions/Instructions" 
+import Instructions from "./components/Instructions/Instructions"
 import Chip from "./components/Chip/Chip"
+import NutritionalLabel from "./components/NutritionalLabel/NutritionalLabel"
 
 // don't move this!
 export const appInfo = {
@@ -17,7 +18,7 @@ export const appInfo = {
     start: `Start by clicking on a food category on the left and a fast food joint from the list above. Afterwards, you'll be able to choose from a list of menu items and see their nutritional content.`,
     onlyCategory: `Now select a fast food restaurant from the list above!`,
     onlyRestaurant: `Now select a category from the list on the left!`,
-    noSelectedItem: `Almost there! Choose a menu item and you'll have the fast food facts right at your fingertips!`,
+    noSelectedItem: `Almost there! Choose a menu element and you'll have the fast food facts right at your fingertips!`,
     allSelected: `Great choice! Amazing what a little knowledge can do!`,
   },
 }
@@ -26,10 +27,16 @@ const { data, categories, restaurants } = createDataSet()
 
 export function App() {
 
-  
 
-  const [category, setCategory] = React.useState(0);
+
   const [restaurant, setRestaurant] = React.useState(0);
+  const [category, setCategory] = React.useState(0);
+  // first get current restaurant items
+  const currentMenuItems = data.filter(e => {return (e.restaurant === restaurant) && (e.food_category === category)});
+  
+  const [menuItem, setMenuItem] = React.useState(0);
+  console.log(menuItem);
+  console.log(currentMenuItems);
 
   return (
     <main className="App">
@@ -38,10 +45,10 @@ export function App() {
         <div className="categories options">
           <h2 className="title">Categories</h2>
           {/* iterate over all food types to be displayed */}
-          {categories.map((item, i) => (
-            <Chip key={i} label={item} isActive={category === item} onClick={() =>
-              setCategory(item)
-            }/>
+          {categories.map((element, i) => (
+            <Chip key={i} label={element} isActive={category === element} onClick={() =>
+              setCategory(element)
+            } />
           ))}
 
         </div>
@@ -49,31 +56,37 @@ export function App() {
 
       {/* MAIN COLUMN */}
       <div className="container">
-        {<Header title={appInfo.title} tagline={appInfo.tagline} description={appInfo.description}/>}
+        {<Header title={appInfo.title} tagline={appInfo.tagline} description={appInfo.description} />}
 
         {/* RESTAURANTS ROW */}
         <div className="RestaurantsRow">
           <h2 className="title">Restaurants</h2>
           <div className="restaurants options">
-            {restaurants.map((item, i) => (
-              <Chip key={i} label={item} isActive={restaurant === item} onClick={() => 
-                setRestaurant(item)
-              }/>
+            {restaurants.map((element, i) => (
+              <Chip key={i} label={element} isActive={restaurant === element} onClick={() =>
+                setRestaurant(element)
+              } />
             ))}
           </div>
         </div>
 
-        <Instructions instructions={appInfo.instructions.start}/>
+        <Instructions instructions={appInfo.instructions.start} />
 
         {/* MENU DISPLAY */}
         <div className="MenuDisplay display">
           <div className="MenuItemButtons menu-items">
             <h2 className="title">Menu Items</h2>
-            {/* YOUR CODE HERE */}
+            {currentMenuItems.map((element, i) => (
+              <Chip key={i} label={element.item_name} isActive={menuItem === element} onClick={() => (
+                setMenuItem(element)
+              )} />
+            ))}
           </div>
 
           {/* NUTRITION FACTS */}
-          <div className="NutritionFacts nutrition-facts">{/* YOUR CODE HERE */}</div>
+          <div className="NutritionFacts nutrition-facts">{
+            <NutritionalLabel item={menuItem}></NutritionalLabel>
+          }</div>
         </div>
 
         <div className="data-sources">
--- src/components/NutritionalLabel/NutritionalLabel.jsx
+++ src/components/NutritionalLabel/NutritionalLabel.jsx
@@ -7,18 +7,21 @@ export function NutritionalLabel(props) {
     <div className="nutritional-label">
       <h3 className="title">Nutrition Facts</h3>
 
-      <h4 className="item-name">{`CHANGE_ME`}</h4>
+      <h4 className="item-name">{props.item.item_name}</h4>
 
-      <ul className="fact-list">{/* WRITE CODE HERE */}</ul>
+      <ul className="fact-list">{nutritionFacts.map((el, index) =>
+        <NutritionalLabelFact id={el.id} label={el.label} attribute={el.attribute} item={props.item}/>
+      )}</ul>
     </div>
   )
 }
 
 export function NutritionalLabelFact(props) {
+
   return (
     <li className="nutrition-fact">
-      <span className="fact-label">{/* WRITE CODE HERE */}</span>{" "}
-      <span className="fact-value">{/* WRITE CODE HERE */}</span>
+      <span className="fact-label">{props.label}</span>{" "}
+      <span className="fact-value">{props.item[props.attribute]}</span>
     </li>
   )
 }
